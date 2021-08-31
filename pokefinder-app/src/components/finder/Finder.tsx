97aed6be8cd0e5f03a90f6a3b35eaf0882e91b75
Cristian Rios <rios7563@gmail.com>
2021-08-31 04:02:44 -0300

    Define base Layout for pokeweb

---
+++ pokefinder-app/src/components/finder/Finder.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Finder.css";
+
+const Finder = () => {
+  return (
+    <>
+      <div className="container">Finder</div>
+      <div className="separator">
+        <div className="dot" />
+      </div>
+    </>
+  );
+};
+
+export default Finder;
---
+++ pokefinder-app/src/components/resultsList/ResultsList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ResultsList.css";
+
+const ResultsList = () => {
+  return <div className="resultsContainer">Pokemons!</div>;
+};
+
+export default ResultsList;
