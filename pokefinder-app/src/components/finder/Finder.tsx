26ad94d8c4a8cf42a51b6fb36dc826a673a84861
Cristian Rios <rios7563@gmail.com>
2021-08-31 07:49:23 -0300

    Finder as form

--- pokefinder-app/src/components/finder/Finder.tsx
+++ pokefinder-app/src/components/finder/Finder.tsx
@@ -1,10 +1,4 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Input, Text
-} from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Input, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import "./Finder.css";
@@ -20,20 +14,24 @@ const Separator = () => {
 const PokemonForm = () => {
   const [pokemon, setPokemon] = useState("");
   const dispatch = useDispatch();
-  const handleSubmit = () =>
+  const handleSubmit = (event: React.FormEvent) => {
     dispatch({ type: "QUERY_POKEMON", payload: pokemon });
+    event.preventDefault();
+  };
   return (
-    <Flex>
-      <Input
-        className="input"
-        placeholder="Ingrese el nombre a buscar"
-        onChange={(event) => setPokemon(event?.target?.value)}
-      />
-      <Box w="1%" />
-      <Button className="button" onClick={handleSubmit}>
-        Buscar
-      </Button>
-    </Flex>
+    <form onSubmit={(event) => handleSubmit(event)}>
+      <Flex>
+        <Input
+          className="input"
+          placeholder="Ingrese el nombre a buscar"
+          onChange={(event) => setPokemon(event?.target?.value)}
+        />
+        <Box w="1%" />
+        <Button className="button" type="submit">
+          Buscar
+        </Button>
+      </Flex>
+    </form>
   );
 };
 
